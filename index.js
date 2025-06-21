be803f721ce9c350496549e70f28b7aac503f418
Syed Abdullah Ahmed <126831585+SyedAbdullahAhmed@users.noreply.github.com>
2025-06-21 14:24:04 -0400

    Some changed

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const cardController = require('./routes/card.route')
 const keysController = require('./routes/keys.route')
 const paymentController = require('./routes/payment.route')
 const notificationsController = require('./routes/notifications.route')
+const { verifyJWT } = require("./middlewares/auth")
 // const generalController = require('./controllers/general.controller')
 const createTransactionReceipt = require("./utils/createTransactionRecepit")
 
@@ -46,6 +47,22 @@ app.use('/api/notifications', notificationsController)
 app.get('/', async (req, res) => {
   res.send('server is up!')
 })
+
+app.post('/api/verify-user', verifyJWT, async (req, res) => {
+  if (!req.user) {
+    return res.status(400).json({
+      success: false,
+      data: null,
+      message: "User not found"
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    data: req.user,
+    message: "User found!"
+  });
+});
 
 
 app.listen(port, () => {
